093b6a0da31e1b841738c8f018ceeaeec96b9588
Anmol Gupta <111723339+anmolgupta313@users.noreply.github.com>
2025-02-10 16:07:12 -0500

    I have added more funcyions to the widget like adding tab based on url and add widget

--- src/app/Components/Widget.jsx
+++ src/app/Components/Widget.jsx
@@ -7,65 +7,105 @@ function Widget() {
   const [tab, setTab] = useState([]);
   const [inputValue, setInputValue] = useState("");
   const [frameUrl, setFrameUrl] = useState(null);
-const[widget,setWidget]=useState([])
+  const [widget, setWidget] = useState([]);
   const iframeDivRef = useRef(null); // Added ref for iframe container
+
+  const addWidget = () => {
+    return setWidget((prev) => {
+      return [
+        ...prev,
+        {
+          widgetId: widget.length + 1,
+          WidgetTabs: [],
+          frameUrl: "",
+        },
+      ];
+    });
+  };
 
   const addTab = (e) => {
     e.preventDefault();
-    setTab((prev) => [
-      ...prev,
-      {id:widget.length,
-        widgetTab:[
-             { tabId: tab.length + 1,
-        query: inputValue,}
-        ]
-      
-      },
-    ]);
 
-    // setFrameUrl(inputValue);
+    setTab((prev) => {
+      const exsistingTab = prev.find((tab) => {
+        // console.log(tab,"tabfindvalue")
+        return tab.id == widget.length;
+      });
+
+      if (exsistingTab) {
+        return prev.map((tab) => {
+          // console.log(tab,"prevtab")
+          return tab.id == widget.length
+            ? {
+                ...tab,
+                widgetTab: [
+                  ...tab.widgetTab,
+                  { tabId: tab.widgetTab.length + 1, query: inputValue },
+                ],
+              }
+            : "";
+        });
+      } else {
+        return [
+          ...prev,
+
+          {
+            id: widget.length,
+            widgetTab: [{ tabId: tab.length + 1, query: inputValue }],
+          },
+        ];
+      }
+    });
+
+    // setFrameUrl(() => {
+    //   return tab.map((tabQuery) => {
+    //     return tabQuery.widgetTab.map((query) => {
+    //       return query.query;
+    //     });
+    //   });
+    // });
     setInputValue("");
+
+    setWidget((prev) => {
+      return prev.map((w) => {
+        if (w.widgetId === widget.length) {
+          return {
+            ...w,
+            WidgetTabs: [
+              ...w.WidgetTabs,
+              { tabId: w.WidgetTabs.length + 1, query: inputValue },
+            ],
+            frameUrl: inputValue,
+          };
+        }
+        return w;
+      });
+    });
   };
 
-  const tabValue = (e, tabs) => {
-    settabBtnValue(e.target.value);
-    if (e.target.value == tabs.tabId) {
-      setFrameUrl(tabs.query);
-    } else {
-      setFrameUrl("fail");
-    }
+  const tabValue = (e, widgetId, tabs) => {
+    setWidget((prev) => {
+      return prev.map((w) => {
+        return w.widgetId === widgetId ? { ...w, frameUrl: tabs.query } : w;
+      });
+    });
+    // settabBtnValue(e.target.value);
+    // if (e.target.value == tabs.tabId) {
+    //   setFrameUrl(tabs.query);
+    // } else {
+    //   setFrameUrl("fail");
+    // }
   };
 
   const onChange = (e) => {
     setInputValue(e.target.value);
   };
 
+  console.log(tab, "tabss");
 
-  function w(){
-    const f= tab.filter((tabs)=>{
-return tabs.id===widget.length
-    })
-
-    const m=f.map((tabss)=>{
-console.log(tabss.widgetTab,"tabsswidget")
-    })
-
-    console.log(f,"F")
-  }
-
-  // w()
-
-  const addWidget=()=>{
-    return setWidget((prev)=>{
-return [...prev,{widgetId:widget.length+1,  WidgetTabs: tab.filter((tabss) => tabss.id === prev.length).map((tabss) => tabss.widgetTab)},
-]
-    })
-  }
-  console.log(tab,"tabss")
-
-  // console.log(widget,"widget")
+  console.log(widget, "widget");
   return (
-    <div className="main-widget-div p-4">
+    <div className="main-widget-div p-4" style={{ marginTop: "50%" }}>
       <button onClick={addWidget}>Add Widget</button>
       <div className="input-div my-2">
         <input
@@ -82,10 +122,25 @@ return [...prev,{widgetId:widget.length+1,  WidgetTabs: tab.filter((tabss) => ta
           Add
         </button>
       </div>
-      {frameUrl && (
+
+      <div
+        className="main-div-tab-iframe"
+        ref={iframeDivRef}
+        style={{
+          width: `400px`,
+          height: `300px`,
+          position: "relative",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          cursor: "move",
+          overflow: "hidden", // Prevent content overflow during resize
+        }}
+      >
+
         <div
-          className="main-div-tab-iframe"
           ref={iframeDivRef}
+          className="iframe-div border"
           style={{
             width: `400px`,
             height: `300px`,
@@ -97,45 +152,41 @@ return [...prev,{widgetId:widget.length+1,  WidgetTabs: tab.filter((tabss) => ta
             overflow: "hidden", // Prevent content overflow during resize
           }}
         >
-          <div className="tabs-div flex gap-2 mb-4">
-            {tab.map((tabs) => (
-              <button
-                key={tabs.tabId}
-                onClick={(e) => tabValue(e, tabs)}
-                value={tabs.tabId}
-                className="px-3 py-1 bg-gray-300 rounded hover:bg-gray-400"
-              >
-                Tab {tabs.tabId}
-              </button>
-            ))}
-          </div>
+          {widget.map((widget) => {
+            return (
+              <div className="widget-main-div">
+                {tab && (
+                  <div className="widget-tab-div">
+                    {widget.WidgetTabs.map((tabs) => (
+                      <button
+                        key={tabs.tabId}
+                        onClick={(e) => tabValue(e, widget.widgetId, tabs)}
+                        value={tabs.tabId}
+                        className="px-3 py-1 bg-gray-300 rounded hover:bg-gray-400"
+                      >
+                        Tab {tabs.tabId}
+                      </button>
+                    ))}
+                  </div>
+                )}
+                <div className="widget-i-frame-div">
+                  {" "}
+                  <iframe
+                    src={widget.frameUrl}
+                    title="Embedded Content"
+                    style={{
+                      width: "100%", // Make iframe fill the parent
+                      height: "100%",
+                      border: "none",
+                    }}
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
 
-          <div
-            ref={iframeDivRef}
-            className="iframe-div border"
-            style={{
-              width: `400px`,
-              height: `300px`,
-              position: "relative",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              cursor: "move",
-              overflow: "hidden", // Prevent content overflow during resize
-            }}
-          >
-            <iframe
-              src={frameUrl}
-              title="Embedded Content"
-              style={{
-                width: "100%", // Make iframe fill the parent
-                height: "100%",
-                border: "none",
-              }}
-            />
-          </div>
-
-          <Moveable
+        {/* <Moveable
             target={iframeDivRef.current} // Target using ref
             origin={true}
             edge={false}
@@ -158,9 +209,8 @@ return [...prev,{widgetId:widget.length+1,  WidgetTabs: tab.filter((tabss) => ta
             keepRatio={true}
             resizable={true}
             throttleResize={0}
-          />
-        </div>
-      )}
+          /> */}
+      </div>
     </div>
   );
 }
